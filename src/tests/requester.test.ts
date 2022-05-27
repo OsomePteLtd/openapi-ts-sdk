620cd314be167ea93b433f8e6b9524ac2ad1fa52
Chingis Gomboev <chg@osome.com>
2022-05-27 19:51:17 +0800

    sss

--- src/tests/requester.test.ts
+++ src/tests/requester.test.ts
@@ -44,17 +44,54 @@ describe('get', () => {
     const baseUrl = 'https://example.com';
     const requester = new SdkRequester({ baseUrl });
     const expectedUrl = '/';
-    const getNock = nock(baseUrl).get(expectedUrl).delayConnection(2000).reply(200);
-    const request = requester.get(
-      '/',
-      {},
-      { cancelToken: source.token },
-    );
+    const getNock = nock(baseUrl)
+      .get(expectedUrl)
+      .delayConnection(2000)
+      .reply(200);
+    const request = requester.get('/', {}, { cancelToken: source.token });
     source.cancel(`cancelled by client`);
 
     await expect(request).rejects.toEqual({
       message: cancellationMessage,
     });
     expect(getNock.isDone()).toBeFalsy();
+  });
+
+  it('catch request & response interceptors', async () => {
+    const baseUrl = 'https://example.com';
+    const requester = new SdkRequester({ baseUrl });
+    let requestCatched = false,
+      responseCatched = false;
+    requester.setInterceptors({
+      request: {
+        onFulfilled: (request) => {
+          expect(request.baseURL).toBe(baseUrl);
+          requestCatched = true;
+          return request;
+        },
+      },
+      response: {
+        onFulfilled: (response) => {
+          expect(response.status).toBe(200);
+          responseCatched = true;
+          return response;
+        },
+      },
+    });
+
+    const expectedUrl = encodeURI(
+      '/path?filter[companyId]=1234&filter[status][]=active&filter[status][]=inactive',
+    );
+    const getNock = nock(baseUrl).get(expectedUrl).reply(200);
+
+    await requester.get('/path', {
+      filter: {
+        companyId: 1234,
+        status: ['active', 'inactive'],
+      },
+    });
+    expect(getNock.isDone()).toBeTruthy();
+    expect(requestCatched).toBeTruthy();
+    expect(responseCatched).toBeTruthy();
   });
 });
